b33c2d9ec270751df170941d92d9225c67311a89
Shanu Karan <Shanu.karan.07@gmail.com>
2022-05-30 14:37:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+import { Routes,Route } from 'react-router-dom';
+import Products from './components/Products';
+import Loginpage from './components/Loginpage';
+import Invalid from './components/Invalid';
+import Cart from './components/Cart';
+import Imagecontainer from './components/Imagecontainer';
+
+export  const Addcontext= React.createContext()
 
 function App() {
+  const [item, setItem] = useState([]);
+
+  const handelcart=(cartitem)=>{
+    setItem([cartitem,...item])
+    console.log(item)
+  }
+  const removecarthandler=(removeitem)=>{
+    const filterdata=item.filter(el=>el.id_product!==removeitem.id_product)
+    setItem(filterdata)
+  }
+
   return (
+    <Addcontext.Provider value={{items:item, addtocart:handelcart, removetocart:removecarthandler}}>
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Routes>
+        <Route path='/' element={<Loginpage />}/>
+        <Route path='home' element={<Products />}/>
+        <Route path='cart' element={<Cart/>}/> 
+        <Route path='img' element={<Imagecontainer/>}/>       
+        <Route path='*' element={<Invalid />}/>
+      </Routes>
     </div>
+    </Addcontext.Provider>
   );
 }
 
-export default App;
+export default App
